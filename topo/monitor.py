fd06298d46af681bd91fa7342727a806b31e201b
Petar Penkov <ppenkov@stanford.edu>
2017-05-28 15:21:30 -0700

    Added monitor script

---
+++ topo/monitor.py
@@ -0,0 +1,10 @@
+from subprocess import *
+
+default_dir = '.'
+
+def monitor_devs_ng(fname="%s/txrate.txt" % default_dir, interval_sec=0.01):
+    """Uses bwm-ng tool to collect iface tx rate stats.  Very reliable."""
+    cmd = ("sleep 1; bwm-ng -t %s -o csv "
+           "-u bits -T rate -C ',' > %s" %
+           (interval_sec * 1000, fname))
+    Popen(cmd, shell=True).wait()
