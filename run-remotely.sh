3d5b653fc5098b017c8b0b52ab721d7cc0fc3bd2
Hristo Stoyanov <renegat96@users.noreply.github.com>
2017-06-03 16:19:25 -0700

    Update run-remotely.sh

--- run-remotely.sh
+++ run-remotely.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # set up the VM_IP
-export VM_IP=`gcloud --format="value(networkInterfaces[0].accessConfigs[0].natIP)" compute instances list -r pcc`
+export VM_IP=`gcloud --format="value(networkInterfaces[0].accessConfigs[0].natIP)" compute instances list -r pcc` ;
 
 # will ssh into the machine, and set it up
-ssh mininet@${VM_IP} -C "wget https://raw.githubusercontent.com/petarpenkov/cs244_pcc/master/setup.sh && bash setup.sh"
+ssh mininet@${VM_IP} -C "wget https://raw.githubusercontent.com/petarpenkov/cs244_pcc/master/setup.sh && bash setup.sh" ;
 
+echo 'Setup done';
 # will run the experiments
-ssh mininet@${VM_IP} -C "screen -dm bash -c 'cd cs244_pcc; pwd; ./run-experiment.sh'"
+
+ssh mininet@${VM_IP} -C "screen -dm bash -c 'cd cs244_pcc; pwd; ./run-experiment.sh'" ;
+
+echo 'Running experiment';
